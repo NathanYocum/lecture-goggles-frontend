fac35d295a901b115a57182382fe33036fa7167c
Nathan Yocum <nayocum@nevada.unr.edu>
2019-01-24 15:22:43 -0800

    Started tests of React Router

--- src/index.js
+++ src/index.js
@@ -7,28 +7,15 @@
 /* eslint react/jsx-filename-extension: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Switch from 'react-router-dom/Switch';
-import LandingPage from './components/LandingPage/LandingPage';
-import NotFound from './components/NotFound/NotFound';
-import Resources from './components/Resources/Resources';
-import DevelopersPage from './components/Developers/Developers';
-import SignIn from './components/SignIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import App from './App';
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/resources" component={Resources} />
-      <Route exact path="/developers" component={DevelopersPage} />
-      <Route exact path="/signIn" component={SignIn} />
-      <Route exact path="/newAccount" component={SignUp} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
 );
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-export default App;
