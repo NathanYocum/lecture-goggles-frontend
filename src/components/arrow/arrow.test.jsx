1d7f81bdb5e4c006ecbbc32f6782be9ebffcd3ea
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-04 17:06:43 -0800

    Changed test from role to data-testid

--- src/components/arrow/arrow.test.jsx
+++ src/components/arrow/arrow.test.jsx
@@ -6,7 +6,7 @@ afterEach(cleanup);
 
 it('Add an onClick function to the button', () => {
   const fn = jest.fn();
-  const { getByRole } = render(<ArrowButton onClickFunction={fn} />);
-  fireEvent.click(getByRole('button'), { button: 0 });
+  const { queryByTestId } = render(<ArrowButton onClickFunction={fn} />);
+  fireEvent.click(queryByTestId('arrow-button'), { button: 0 });
   expect(fn).toHaveBeenCalledTimes(1);
 });
