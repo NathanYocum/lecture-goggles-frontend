eff3bdc13e3ed9c4db82d759d5c11af5d67e82f5
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-06 10:27:24 -0800

    Started Formik for SignIn. Made SignIn use hooks

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import styled from 'styled-components';
+import { Formik } from 'formik';
 import NavBar from '../navBar/navBar';
 import LectureGogglesLogo from '../logo/logo';
 import GenericButton from '../button/button';
 import GridBody from '../gridBody';
+import useWindowWidth from '../__hooks__/useWindowWidth';
 
 const LogoStyle = styled.div`
   grid-column: 2;
@@ -19,6 +23,7 @@ const WelcomeStyle = styled.div`
   background-color: #ffffff;
   color: #0d47a1;
   padding: 20px;
+  width: 100%;
 `;
 
 const ContinueButtonStyle = styled.div`
@@ -30,67 +35,49 @@ const ContinueButtonStyle = styled.div`
   margin: 30px;
 `;
 
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: 0
-    };
-    this.getWindowWidth = this.getWindowWidth.bind(this);
-  }
+const InputStyle = styled.input`
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+  border-radius: 4px;
+  border: 1px solid #0074d9;
+  text-align: center;
+  height: 56px;
+`;
 
-  componentDidMount() {
-    this.getWindowWidth();
-    window.addEventListener('resize', this.getWindowWidth);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.getWindowWidth);
-  }
-
-  getWindowWidth() {
-    this.setState({ width: window.innerWidth });
-  }
-
-  render() {
-    const { width } = this.state;
-    return (
-      <GridBody data-testid="sign-in">
-        <NavBar renderButton={width < 768} />
-        <LogoStyle>
-          <LectureGogglesLogo width={200} height={200} />
-        </LogoStyle>
-        <WelcomeStyle>
-          <h3> Sign In </h3>
-          <form>
-            <label htmlFor="firstName">
-              Email
+const SignIn = () => {
+  const width = useWindowWidth();
+  return (
+    <GridBody data-testid="sign-in">
+      <NavBar renderButton={width < 768} />
+      <LogoStyle>
+        <LectureGogglesLogo width={200} height={200} />
+      </LogoStyle>
+      <WelcomeStyle>
+        <h3> Sign In </h3>
+        <Formik
+          render={() => (
+            <form>
+              <InputStyle placeholder="email" type="text" name="email" />
               <br />
-              <input type="text" name="firstName" />
-            </label>
-            <br />
-            <label htmlFor="firstName">
-              Password
-              <br />
-              <input type="password" name="firstName" />
-              <br />
+              <InputStyle placeholder="password" type="password" name="password" />
               <a href="/">Forgot your password?</a>
-            </label>
-            <br />
-          </form>
-        </WelcomeStyle>
-        <ContinueButtonStyle>
-          <GenericButton text="Continue" />
-          <a href="/">
-            <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Cancel" />
-          </a>
-          <a href="/newAccount">
-            <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Create An Account" />
-          </a>
-        </ContinueButtonStyle>
-      </GridBody>
-    );
-  }
-}
+              <br />
+              <GenericButton width="100%" height="56px" type="submit" text="Continue" />
+            </form>
+          )}
+        />
+      </WelcomeStyle>
+      <ContinueButtonStyle>
+        <a href="/">
+          <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Cancel" />
+        </a>
+        <a href="/newAccount">
+          <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Create An Account" />
+        </a>
+      </ContinueButtonStyle>
+    </GridBody>
+  );
+};
 
 export default SignIn;
