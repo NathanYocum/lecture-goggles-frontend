24e005363716d52d3f6252f93310642a5db16da9
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-06 10:40:15 -0800

    Started SignUp rewrite. Added hooks.

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import styled from 'styled-components';
 import NavBar from '../navBar/navBar';
 import LectureGogglesLogo from '../logo/logo';
 import GenericButton from '../button/button';
 import GridBody from '../gridBody';
+import useWindowWidth from '../__hooks__/useWindowWidth';
 
 const LogoStyle = styled.div`
   grid-column: 2;
@@ -30,101 +33,95 @@ const ContinueButtonStyle = styled.div`
   margin: 30px;
 `;
 
-class SignUp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: 0
-    };
-    this.getWindowWidth = this.getWindowWidth.bind(this);
-  }
+const LabelStyle = styled.label`
+  text-align: center;
+  font-size: 24px;
+  display: block;
+`;
 
-  componentDidMount() {
-    this.getWindowWidth();
-    window.addEventListener('resize', this.getWindowWidth);
-  }
+const InputStyle = styled.input`
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+  border-radius: 4px;
+  border: 1px solid #0074d9;
+  text-align: center;
+  height: 56px;
+  font-size: 24px;
+`;
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.getWindowWidth);
-  }
-
-  getWindowWidth() {
-    this.setState({ width: window.innerWidth });
-  }
-
-  render() {
-    const { width } = this.state;
-    return (
-      <GridBody data-testid="sign-up">
-        <NavBar renderButton={width < 768} />
-        <LogoStyle>
-          <LectureGogglesLogo width={200} height={200} />
-          <p>Lecture Goggles</p>
-        </LogoStyle>
-        <WelcomeStyle>
-          <form>
-            <label htmlFor="firstName">
-              First Name
-              <br />
-              <input type="text" name="firstName" />
-            </label>
+const SignUp = () => {
+  const width = useWindowWidth();
+  return (
+    <GridBody data-testid="sign-up">
+      <NavBar renderButton={width < 768} />
+      <LogoStyle>
+        <LectureGogglesLogo width={200} height={200} />
+        <p>Lecture Goggles</p>
+      </LogoStyle>
+      <WelcomeStyle>
+        <form>
+          <LabelStyle htmlFor="firstName">
+            First Name
             <br />
-            <label htmlFor="firstName">
-              Last Name
-              <br />
-              <input type="text" name="firstName" />
-            </label>
+            <InputStyle type="text" name="firstName" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="lastName">
+            Last Name
             <br />
-            <label htmlFor="firstName">
-              Email
-              <br />
-              <input type="text" name="firstName" />
-            </label>
+            <InputStyle type="text" name="lastName" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="email">
+            Email
             <br />
-            <label htmlFor="firstName">
-              Confirm Email
-              <br />
-              <input type="text" name="firstName" />
-            </label>
+            <InputStyle type="text" name="email" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="confirmEmail">
+            Confirm Email
             <br />
-            <label htmlFor="firstName">
-              Password
-              <br />
-              <input type="password" name="firstName" />
-            </label>
+            <InputStyle type="text" name="confirmEmail" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="password">
+            Password
             <br />
-            <label htmlFor="firstName">
-              Confirm Password
-              <br />
-              <input type="password" name="firstName" />
-            </label>
+            <InputStyle type="password" name="password" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="confirmPassword">
+            Confirm Password
             <br />
-            <label htmlFor="firstName">
-              Institution
-              <br />
-              <input type="text" name="firstName" />
-            </label>
+            <InputStyle type="password" name="confirmPassword" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="institution">
+            Institution
             <br />
-            <label htmlFor="firstName">
-              Are you an instructor at your institution?
-              <br />
-              <input type="radio" name="firstName" />
-              Yes
-              <input type="radio" name="firstName" />
-              No
-            </label>
+            <InputStyle type="text" name="institution" />
+          </LabelStyle>
+          <br />
+          <LabelStyle htmlFor="firstName">
+            Are you an instructor at your institution?
             <br />
-          </form>
-        </WelcomeStyle>
-        <ContinueButtonStyle>
-          <GenericButton text="Continue" />
-          <a href="/">
-            <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Cancel" />
-          </a>
-        </ContinueButtonStyle>
-      </GridBody>
-    );
-  }
-}
+            <input type="radio" name="firstName" />
+            Yes
+            <input type="radio" name="firstName" />
+            No
+          </LabelStyle>
+          <br />
+          <ContinueButtonStyle>
+            <GenericButton type="submit" text="Continue" />
+            <a href="/">
+              <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Cancel" />
+            </a>
+          </ContinueButtonStyle>
+        </form>
+      </WelcomeStyle>
+    </GridBody>
+  );
+};
 
 export default SignUp;
