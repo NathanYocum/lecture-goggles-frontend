fdeaf201204b65320c7a49b0cb1a69515f211f35
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-10 22:21:59 -0800

    Added tests to sign in

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -27,12 +27,14 @@ const WelcomeStyle = styled.div`
 `;
 
 const ContinueButtonStyle = styled.div`
-  grid-row: 5;
   grid-column: 2;
   background-color: #ffffff;
   justify-self: center;
   font-size: 32px;
   margin: 30px;
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
 `;
 
 const InputStyle = styled.input`
@@ -58,6 +60,7 @@ const SignInSchema = Yup.object().shape({
 
 const SignIn = () => {
   const width = useWindowWidth();
+  const [isButtonDisabled, setButtonDisabled] = React.useState(true);
   return (
     <GridBody data-testid="sign-in">
       <NavBar renderButton={width < 768} />
@@ -71,10 +74,11 @@ const SignIn = () => {
           validationSchema={SignInSchema}
           render={props => {
             // eslint-disable-next-line
-            const { handleBlur, handleChange, values, errors } = props;
+            const { handleBlur, handleChange, values, errors, isSubmitting } = props;
             return (
               <form>
                 <InputStyle
+                  data-testid="sign-in-email-input"
                   placeholder="email"
                   onChange={handleChange}
                   onBlur={handleBlur}
@@ -83,25 +87,41 @@ const SignIn = () => {
                   name="email"
                   emailError={errors.email}
                 />
-                <ErrorDiv>{errors.email}</ErrorDiv>
+                {errors.email ? (
+                  <ErrorDiv data-testid="sign-in-email-error">
+                    {errors.email} {setButtonDisabled(true)}
+                  </ErrorDiv>
+                ) : (
+                  setButtonDisabled(false)
+                )}
                 <br />
-                <InputStyle placeholder="password" type="password" name="password" />
+                <InputStyle
+                  data-testid="sign-in-password-input"
+                  placeholder="password"
+                  type="password"
+                  name="password"
+                />
                 <a href="/">Forgot your password?</a>
                 <br />
-                <GenericButton width="100%" height="56px" type="submit" text="Continue" />
+                <ContinueButtonStyle>
+                  <GenericButton width="100%" height="56px" type="submit" text="Continue" />
+                  <a href="/">
+                    <GenericButton
+                      disabled={isSubmitting || isButtonDisabled}
+                      backgroundColor="#90A4AE"
+                      color="#0D47A1"
+                      text="Cancel"
+                    />
+                  </a>
+                  <a href="/newAccount">
+                    <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Create An Account" />
+                  </a>
+                </ContinueButtonStyle>
               </form>
             );
           }}
         />
       </WelcomeStyle>
-      <ContinueButtonStyle>
-        <a href="/">
-          <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Cancel" />
-        </a>
-        <a href="/newAccount">
-          <GenericButton backgroundColor="#90A4AE" color="#0D47A1" text="Create An Account" />
-        </a>
-      </ContinueButtonStyle>
     </GridBody>
   );
 };
--- src/components/SignIn/SignIn.test.jsx
+++ src/components/SignIn/SignIn.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent, wait } from 'react-testing-library';
 import SignIn from './SignIn';
+import 'jest-dom/extend-expect';
 
 afterEach(cleanup);
 
@@ -14,4 +15,16 @@ it('Renders a hamburger menu at 768px', () => {
   global.innerWidth = 768;
   const { queryByTestId } = render(<SignIn />);
   expect(queryByTestId(/hamburger-button/gm)).toBeNull();
+});
+
+it('Renders an error when I enter an invalid email', async () => {
+  const { queryByTestId } = render(<SignIn />);
+  fireEvent.change(queryByTestId('sign-in-email-input'), { target: { value: 'John' } });
+  await wait(() => expect(queryByTestId('sign-in-email-error')).toBeInTheDocument());
+});
+
+it('Does not render an error when I enter an valid email', async () => {
+  const { queryByTestId } = render(<SignIn />);
+  fireEvent.change(queryByTestId('sign-in-email-input'), { target: { value: 'John@Deer.com' } });
+  await wait(() => expect(queryByTestId('sign-in-email-error')).not.toBeInTheDocument());
 });
