6313afbf06908ef0ed7ea98eb9d155ea22789d5a
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-12 19:41:56 -0800

    Added upload tests

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -20,8 +20,9 @@ const FormContainer = styled.div`
 const BG = styled.div`
   grid-column: 1 / span 3;
   grid-row: 1 / span 2;
-  background-color: #0074d9;
+  background-color: #004aa7;
   width: 100%;
+  height: 300px;
 `;
 
 const InputStyle = styled.input`
@@ -47,8 +48,12 @@ const TextAreaStyle = styled.textarea`
   border: 1px solid #0074d9;
   min-width: 80%;
   resize: vertical;
-  text-align: center;
   height: 56px;
+  border: ${props => (props.hasErrors ? '1px solid #ff4136' : '1px solid #0074d9')};
+
+  :focus {
+    outline-color: ${props => (props.hasErrors ? '#ff4136' : '#0074d9')};
+  }
 `;
 
 const SelectStyle = styled.select`
@@ -65,6 +70,7 @@ const LabelStyle = styled.label`
   font-size: 24px;
   display: block;
   color: #0074d9;
+  margin-top: 10px;
 `;
 
 const ErrorDiv = styled.div`
@@ -81,11 +87,17 @@ const ErrorDiv = styled.div`
 const UploadSchema = Yup.object().shape({
   url: Yup.string()
     .url('Invalid URL')
-    .required('Required')
+    .required('Required'),
+  title: Yup.string()
+    .max(40, "Titles can't be over 40 characters ")
+    .required('Required'),
+  description: Yup.string()
+    .max(240, "Description can't be longer than 240 characters")
+    .notRequired()
 });
 
 const UploadPage = () => {
-  const [isButtonDisabled, setButtonDisabled] = React.useState(false);
+  const [isButtonDisabled, setButtonDisabled] = React.useState(true);
   const width = useWindowWidth();
   return (
     <GridBody data-testid="upload">
@@ -117,16 +129,40 @@ const UploadPage = () => {
                 ) : (
                   setButtonDisabled(false)
                 )}
-                Title
-                <InputStyle type="text" onBlur={handleBlur} onChange={handleChange} value={values.title} name="title" />
-                Description
+                <LabelStyle htmlFor="title">Title</LabelStyle>
+                <InputStyle
+                  data-testid="title-upload-input"
+                  hasErrors={errors.title}
+                  type="text"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.title}
+                  name="title"
+                />
+                {errors.title ? (
+                  <ErrorDiv data-testid="title-upload-error">
+                    {errors.title} {setButtonDisabled(true)}
+                  </ErrorDiv>
+                ) : (
+                  setButtonDisabled(false)
+                )}
+                <LabelStyle htmlFor="description">Description</LabelStyle>
                 <TextAreaStyle
+                  data-testid="description-upload-input"
                   type="text"
                   onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.description}
+                  hasErrors={errors.description}
                   name="description"
                 />
+                {errors.description ? (
+                  <ErrorDiv data-testid="description-upload-error">
+                    {errors.description} {setButtonDisabled(true)}
+                  </ErrorDiv>
+                ) : (
+                  setButtonDisabled(Object.keys(errors).length !== 0)
+                )}
                 Subject
                 <SelectStyle
                   type="text"
@@ -148,7 +184,8 @@ const UploadPage = () => {
                 <br />
                 <GenericButton height="56px" width="40%" text="CANCEL" backgroundColor="#90a4ae" color="#0074d9" />
                 <GenericButton
-                  disabled={isButtonDisabled || isSubmitting}
+                  testId="submit-button"
+                  disabled={isSubmitting || isButtonDisabled}
                   backgroundColor={`${isButtonDisabled ? '#aaaaaa' : '#0074d9'}`}
                   color={`${isButtonDisabled ? '#111111' : '#ffffff'}`}
                   borderColor={`${isButtonDisabled ? '#111111' : '#0d47a1'}`}
--- src/components/Upload/Upload.test.jsx
+++ src/components/Upload/Upload.test.jsx
@@ -22,4 +22,52 @@ it('Renders an error when I input an invalid URL', async () => {
   fireEvent.change(getByTestId('url-upload-input'), { target: { value: 'hi mom!' } });
   await wait(() => expect(getByTestId('url-upload-error')).toBeInTheDocument());
   expect(getByTestId('url-upload-error').innerHTML).toMatch('Invalid URL');
+  expect(getByTestId('submit-button')).toHaveAttribute('disabled');
+});
+
+it('Does not render an error when I input a valid URL', async () => {
+  const { queryByTestId } = render(<UploadPage />);
+  fireEvent.change(queryByTestId('url-upload-input'), { target: { value: 'https://www.github.com' } });
+  await wait(() => expect(queryByTestId('url-upload-error')).not.toBeInTheDocument());
+});
+
+it('Renders an error when I do not put in a title', async () => {
+  const { getByTestId } = render(<UploadPage />);
+  fireEvent.change(getByTestId('title-upload-input'), { target: { value: ' ' } });
+  fireEvent.change(getByTestId('title-upload-input'), { target: { value: '' } });
+  await wait(() => expect(getByTestId('title-upload-error')).toBeInTheDocument());
+  expect(getByTestId('title-upload-error').innerHTML).toMatch('Required');
+  expect(getByTestId('submit-button')).toHaveAttribute('disabled');
+});
+
+it('Renders an error when I do not put in a title that is too long', async () => {
+  const { getByTestId } = render(<UploadPage />);
+  fireEvent.change(getByTestId('title-upload-input'), {
+    target: { value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' }
+  });
+  await wait(() => expect(getByTestId('title-upload-error')).toBeInTheDocument());
+  expect(getByTestId('title-upload-error').innerHTML).toMatch("Titles can't be over 40 characters");
+  expect(getByTestId('submit-button')).toHaveAttribute('disabled');
+});
+
+it('Does not render an error if a description is not included', async () => {
+  const { queryByTestId } = render(<UploadPage />);
+  fireEvent.change(queryByTestId('description-upload-input'), { target: { value: ' ' } });
+  fireEvent.change(queryByTestId('description-upload-input'), { target: { value: '' } });
+  await wait(() => expect(queryByTestId('description-upload-error')).not.toBeInTheDocument());
+});
+
+it('Renders an error when a description is > 240 characters', async () => {
+  const { queryByTestId } = render(<UploadPage />);
+  fireEvent.change(queryByTestId('description-upload-input'), {
+    target: {
+      value: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      aaaaaaaaaaaaaa`
+    }
+  });
+  await wait(() => expect(queryByTestId('description-upload-error')).toBeInTheDocument());
+  expect(queryByTestId('description-upload-error').innerHTML).toMatch(
+    /Description can't be longer than 240 characters/gm
+  );
 });
